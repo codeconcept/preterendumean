0125276d597c5c28d205a332394c381ee62c1c66
codeconcept <contact@codeconcept.fr>
2017-12-03 18:30:38 +0100

    creating a plug layout

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'assets')));
 app.use('/', topPagesRouter);
 
 const port = 3003;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,19 @@ const router = express.Router();
 const users = require('../users');
 
 router.get('/', (req, res) => {
-    res.send('Bienvenue sur Prêté Rendu (le site qui permet de retrouver vos affaires)');
+    res.render('index', { title: 'Accueil'});
 });
 
 router.get('/users', (req, res) => {
     res.json(users);
 });
 
+router.get('/contact', (req, res) => {
+    res.render('../views/contact', { title: 'Contact'});
+});
+
 router.get('/about', (req, res) => {
-    res.render('../views/about');
+    res.render('../views/about', { title: 'A propos'});
 });
 
 module.exports = router;
